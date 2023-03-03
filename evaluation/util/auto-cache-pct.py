e9f2b5bfc65904eeaf2357ca35dd086ea360d11b
Jiali Wang <anlarry@foxmail.com>
2023-03-03 18:23:33 +0800

    script: auto cache search

--- evaluation/util/auto-cache-pct.py
+++ evaluation/util/auto-cache-pct.py
@@ -2,6 +2,9 @@ import subprocess
 import os, sys, re
 import pathlib
 import dataclasses
+from typing import List
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 class Cmd:
     def __init__(self, cmd) -> None:
@@ -21,7 +24,7 @@ class Cmd:
 
     @property
     def dataset(self) -> str: 
-        match = re.search(r'--dataset ([0-9a-zA-Z\-])', self.cmd_tp)
+        match = re.search(r'--dataset ([0-9a-zA-Z\-]+)', self.cmd_tp)
         return match.group(1)
     
     @property
@@ -40,8 +43,8 @@ def extract_cmds(script_path):
         script = re.sub(r'(^.*mkdir.*$)', '', script, flags=re.MULTILINE)
     proc = subprocess.run(args=script, shell=True, executable='/bin/bash', capture_output=True)
     cmds = proc.stdout.decode().strip('\n').split('\n')
-    return [Cmd(cmd) for cmd in cmds]
- 
+    return [Cmd(cmd) for cmd in cmds if 'sgnn' in cmd]
+
 
 def find_cache_pct_impl(cmd:Cmd):
     @dataclasses.dataclass
@@ -52,72 +55,81 @@ def find_cache_pct_impl(cmd:Cmd):
         'twitter' : GraphInfo(5.63, 39.72 ),
         'papers100M' : GraphInfo(6.43, 52.96 ),
         'uk-2006-05' : GraphInfo(11.34, 74.14 ),
-        'com-friendster' : GraphInfo(13.70, 34.22 )
+        'com-friendster' : GraphInfo(13.70, 34.22 ),
+        'products' : GraphInfo(0.47, 0.91 )
     }    
 
+    class UnablePredCacheErr(Exception): ...
     def _get_next_cache_pct(cache_pct, gpu_mem):
         full_mem = 15.8
         if gpu_mem >= full_mem:
             return cache_pct
-        if cmd.is_part_cache() and cmd.num_worker % 2 == 0:
-            avail_mem = (full_mem - gpu_mem) * cmd.num_worker
+        if cmd.is_part_cache() and cmd.num_worker in {2, 4, 8}:
+            avail_mem = (full_mem - gpu_mem) * min(4, cmd.num_worker)
             return min(1, avail_mem / graphs[cmd.dataset].feat_sz + cache_pct)
         elif cmd.is_part_cache() and cmd.num_worker == 1 or not cmd.is_part_cache():
             avail_mem = full_mem - gpu_mem
             return min(1, avail_mem / graphs[cmd.dataset].feat_sz + cache_pct)
         else:
-            raise Exception("cannot predict next cache pct")
+            raise UnablePredCacheErr("cannot predict next cache pct")
 
     def _try_cache_pct(cmd:str):
         proc = subprocess.run(args=cmd.split(), env=os.environ, capture_output=True)
         output = proc.stdout.decode()
         if 'epoch_time:total' in output:
             gpu_memorys = re.findall(r'\| GPU memory ([0-9]+\.[0-9]+)', output)
-            gpu_memory = max([float(m.group(1)) for m in gpu_memorys])
+            gpu_memory = max([float(m) for m in gpu_memorys])
             err = None
         else:
             gpu_memory = None
-            err = proc.stderr.decode()
+            errout = proc.stderr.decode()
+            try:
+                err = re.search(r'\[.* E .*\].*', errout, flags=re.MULTILINE).group(0)
+            except:
+                err = errout
         return proc, gpu_memory, err
 
-    print(f'[TRY CMD]: {cmd.get(cmd.ori_cache_pct)}')
+    logging.info(f'\x1b[32;1m[TRY CMD]\x1b[0m: {cmd.get(cmd.ori_cache_pct)}')
 
     left = 0
     right = 101 if cmd.is_part_cache() else 31
     pred_cache_pct = None
-    while left < right:
+    while left + 1 < right:
         if pred_cache_pct is None:
             cache_pct_int = (left + right) // 2
         else:
             cache_pct_int = pred_cache_pct
         cache_pct = cache_pct_int / 100
         try_cmd = cmd.get(cache_pct)
-        print(f'try {cache_pct_int} in [{left}, {right})')
         proc, gpu_memory, err = _try_cache_pct(try_cmd)
+        msg = f'error: \n{err}' if err is not None else f'gpu memory usage {gpu_memory}'
+        logging.info(f'\x1b[35;1m[TRY RESULT]\x1b[0m: cache_pct {cache_pct_int / 100} in [{left}, {right}), {msg}')
         if not err:
             left = cache_pct_int
             try:
                 nxt_cache_pct = _get_next_cache_pct(cache_pct, gpu_memory)
                 pred_cache_pct = int(100 * nxt_cache_pct)
-                pred_cache_pct = min(right, max(left, pred_cache_pct))
+                pred_cache_pct = min(right - 1, max(left, pred_cache_pct))
                 nxt_bin_mid = (left + right) // 2
+                logging.debug(f'pred_next_cache_pct {pred_cache_pct} binary_search_next_cache_pct {nxt_bin_mid}')
                 if pred_cache_pct <= nxt_bin_mid:
                     pred_cache_pct = None
-            except Exception:
+            except UnablePredCacheErr as e:
                 pred_cache_pct = None
         else:
-            print(err)
             right = cache_pct_int
             pred_cache_pct = None
 
     # check result
-    cache_pct = pred_cache_pct
+    cache_pct = left
     chk_cnter = 0
     while True:
         proc, gpu_memory, err = _try_cache_pct(cmd.get(cache_pct / 100))
-        print(f'check result [{chk_cnter}]: gpu_memory {gpu_memory} error {err}')
-        if not err and gpu_memory >= 15.5:
-            return cache_pct, gpu_memory
+        logging.info(f'\x1b[38;5;208;1m[CHECK RESULT {chk_cnter}]\x1b[0m: cache_pct {cache_pct} gpu_memory {gpu_memory} error {err}')
+        if not err and (gpu_memory >= 15 or cache_pct == 100):
+            return cache_pct / 100, gpu_memory
+        elif err and cache_pct == 0:
+            return cache_pct / 100, gpu_memory
         if err:
             cache_pct -= 1
         else:
@@ -125,30 +137,31 @@ def find_cache_pct_impl(cmd:Cmd):
         chk_cnter += 1
 
 
-def find_cache_pct(cmds):
+def find_cache_pct(cmds:List[Cmd]):
     cache_pcts = []
     gpu_mems = []
     def _output_cache_pct():
+        sys.stdout.flush()
         print(30*'=')
         pct_arr = ""
-        search_res = [f'[CMD] cache-percentage memory-usage']
+        search_res = [f'[CMD] cache-percentage memory-usage\n']
         for i, (cmd, pct, mem) in enumerate(zip(cmds, cache_pcts, gpu_mems)):
-            print(f'[{i:3}] {cmd}')
-            search_res.append(f'[{i:3}] {cache_pct:16} {gpu_mem:12}')
+            print(f'[{i:3}] {cmd.get(cmd.ori_cache_pct)}')
+            search_res.append(f'[{i:3}] {pct:16} {mem:12}\n')
             if (i + 1) % 10 == 0:
-                pct_arr += f'{pct}\n'
+                pct_arr += f'{pct:4}\n'
             elif i % 10 == 0:
-                pct_arr += f'\t{pct} '
+                pct_arr += f'{pct:8} '
             else:
-                pct_arr += f'{pct} '
-        print(f'\n{search_res}\n')
-        print(f'cache_pcts=(\n{pct_arr})')
+                pct_arr += f'{pct:4} '
+        print(f'\n{"".join(search_res)}\n')
+        print(f'cache_pcts=(\n{pct_arr}\n)')
 
     for cmd in cmds:
         cache_pct, gpu_mem = find_cache_pct_impl(cmd)
         cache_pcts.append(cache_pct)
         gpu_mems.append(gpu_mem)
-    _output_cache_pct(cache_pcts)
+    _output_cache_pct()
 
 
 
--- evaluation/util/test.sh
+++ evaluation/util/test.sh
@@ -1,4 +1,4 @@
 python ../../example/samgraph/sgnn/train_gcn.py --use-dist-graph 1 \
     --part-cache --gpu-extract --cache-policy degree --cache-percentage 0  \
     --batch-size 6000  -ll info --num-epoch 3 --dataset products  --num-worker 4 \
-    --sample-type khop3 -pl 3 --pipeline --root-path /disk1/graph-learning-copy/samgraph
+    --sample-type khop3 -pl 3 --pipeline 
