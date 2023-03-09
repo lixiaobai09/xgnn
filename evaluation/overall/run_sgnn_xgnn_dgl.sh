8c0e4289a78daf759d80a35a9308a11a5e7f699f
Dahai Tang <seatang@hnu.edu.cn>
2023-03-09 11:08:46 +0800

    Perf: change cache pert for run overall

--- evaluation/overall/run_sgnn_xgnn_dgl.sh
+++ evaluation/overall/run_sgnn_xgnn_dgl.sh
@@ -10,35 +10,37 @@ log_dir=./run-logs/overall/${TIME_STAMPS}
 
 dgl_devices="0 1 2 3 4 5 6 7"
 num_worker=8
-num_epoch=10
+num_epoch=5
 
 mkdir -p $log_dir
 
+<< EOF
+
 dataset="twitter"
 # for gcn
-python ${dgl_dir}/train_gcn.py --batch-size 6000 --devices ${dgl_devices} --num-epoch ${num_epoch} --dataset ${dataset} --use-gpu-sampling --use-uva-feat > ${log_dir}/dgl_gcn_tw.log 2> ${log_dir}/dgl_gcn_tw.log.err
+# python ${dgl_dir}/train_gcn.py --batch-size 6000 --devices ${dgl_devices} --num-epoch ${num_epoch} --dataset ${dataset} --use-gpu-sampling --use-uva-feat > ${log_dir}/dgl_gcn_tw.log 2> ${log_dir}/dgl_gcn_tw.log.err
 python ${sgnn_dir}/train_gcn.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --sample-type khop0 --gpu-extract --cache-percentage 0.10 > ${log_dir}/sgnn_gcn_tw.log 2> ${log_dir}/sgnn_gcn_tw.log.err
 python ${sgnn_dir}/train_gcn.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.81 > ${log_dir}/xgnn_gcn_tw.log 2> ${log_dir}/xgnn_gcn_tw.log.err
 # # for graphsage
-python ${dgl_dir}/train_graphsage.py --batch-size 6000 --devices ${dgl_devices} --num-epoch ${num_epoch} --dataset ${dataset} --use-gpu-sampling --use-uva-feat > ${log_dir}/dgl_graphsage_tw.log 2> ${log_dir}/dgl_graphsage_tw.log.err
+# python ${dgl_dir}/train_graphsage.py --batch-size 6000 --devices ${dgl_devices} --num-epoch ${num_epoch} --dataset ${dataset} --use-gpu-sampling --use-uva-feat > ${log_dir}/dgl_graphsage_tw.log 2> ${log_dir}/dgl_graphsage_tw.log.err
 python ${sgnn_dir}/train_graphsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --sample-type khop0 --gpu-extract --cache-percentage 0.17 > ${log_dir}/sgnn_graphsage_tw.log 2> ${log_dir}/sgnn_graphsage_tw.log.err
 python ${sgnn_dir}/train_graphsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 1.00 > ${log_dir}/xgnn_graphsage_tw.log 2> ${log_dir}/xgnn_graphsage_tw.log.err
 # # for pinsage
-python ${dgl_dir}/train_pinsage.py --batch-size 6000 --devices ${dgl_devices} --num-epoch ${num_epoch} --dataset ${dataset} --use-gpu-sampling --use-uva-feat > ${log_dir}/dgl_pinsage_tw.log 2> ${log_dir}/dgl_pinsage_tw.log.err
+# python ${dgl_dir}/train_pinsage.py --batch-size 6000 --devices ${dgl_devices} --num-epoch ${num_epoch} --dataset ${dataset} --use-gpu-sampling --use-uva-feat > ${log_dir}/dgl_pinsage_tw.log 2> ${log_dir}/dgl_pinsage_tw.log.err
 python ${sgnn_dir}/train_pinsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --gpu-extract --cache-percentage 0.11 > ${log_dir}/sgnn_pinsage_tw.log 2> ${log_dir}/sgnn_pinsage_tw.log.err
 python ${sgnn_dir}/train_pinsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --gpu-extract --part-cache --use-dist-graph 1.0 --cache-percentage 0.85 > ${log_dir}/xgnn_pinsage_tw.log 2> ${log_dir}/xgnn_pinsage_tw.log.err
 
 dataset="papers100M"
 # for gcn
-python ${dgl_dir}/train_gcn.py --batch-size 6000 --devices ${dgl_devices} --num-epoch ${num_epoch} --dataset ${dataset} --use-gpu-sampling --use-uva-feat > ${log_dir}/dgl_gcn_pa.log 2> ${log_dir}/dgl_gcn_pa.log.err
+# python ${dgl_dir}/train_gcn.py --batch-size 6000 --devices ${dgl_devices} --num-epoch ${num_epoch} --dataset ${dataset} --use-gpu-sampling --use-uva-feat > ${log_dir}/dgl_gcn_pa.log 2> ${log_dir}/dgl_gcn_pa.log.err
 python ${sgnn_dir}/train_gcn.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --sample-type khop0 --gpu-extract --cache-percentage 0.08 > ${log_dir}/sgnn_gcn_pa.log 2> ${log_dir}/sgnn_gcn_pa.log.err
 python ${sgnn_dir}/train_gcn.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.71 > ${log_dir}/xgnn_gcn_pa.log 2> ${log_dir}/xgnn_gcn_pa.log.err
 # for graphsage
-python ${dgl_dir}/train_graphsage.py --batch-size 6000 --devices ${dgl_devices} --num-epoch ${num_epoch} --dataset ${dataset} --use-gpu-sampling --use-uva-feat > ${log_dir}/dgl_graphsage_pa.log 2> ${log_dir}/dgl_graphsage_pa.log.err
+# python ${dgl_dir}/train_graphsage.py --batch-size 6000 --devices ${dgl_devices} --num-epoch ${num_epoch} --dataset ${dataset} --use-gpu-sampling --use-uva-feat > ${log_dir}/dgl_graphsage_pa.log 2> ${log_dir}/dgl_graphsage_pa.log.err
 python ${sgnn_dir}/train_graphsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --sample-type khop0 --gpu-extract --cache-percentage 0.11 > ${log_dir}/sgnn_graphsage_pa.log 2> ${log_dir}/sgnn_graphsage_pa.log.err
 python ${sgnn_dir}/train_graphsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.83 > ${log_dir}/xgnn_graphsage_pa.log 2> ${log_dir}/xgnn_graphsage_pa.log.err
 # for pinsage
-python ${dgl_dir}/train_pinsage.py --batch-size 6000 --devices ${dgl_devices} --num-epoch ${num_epoch} --dataset ${dataset} --use-gpu-sampling --use-uva-feat > ${log_dir}/dgl_pinsage_pa.log 2> ${log_dir}/dgl_pinsage_pa.log.err
+# python ${dgl_dir}/train_pinsage.py --batch-size 6000 --devices ${dgl_devices} --num-epoch ${num_epoch} --dataset ${dataset} --use-gpu-sampling --use-uva-feat > ${log_dir}/dgl_pinsage_pa.log 2> ${log_dir}/dgl_pinsage_pa.log.err
 python ${sgnn_dir}/train_pinsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --gpu-extract --cache-percentage 0.09 > ${log_dir}/sgnn_pinsage_pa.log 2> ${log_dir}/sgnn_pinsage_pa.log.err
 python ${sgnn_dir}/train_pinsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --gpu-extract --part-cache --use-dist-graph 1.0 --cache-percentage 0.73 > ${log_dir}/xgnn_pinsage_pa.log 2> ${log_dir}/xgnn_pinsage_pa.log.err
 
@@ -69,6 +71,7 @@ python ${sgnn_dir}/train_graphsage.py --num-worker ${num_worker} --cache-policy 
 python ${sgnn_dir}/train_pinsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --gpu-extract --part-cache --use-dist-graph 1.0 --cache-percentage 0.28 > ${log_dir}/xgnn_pinsage_cf.log 2> ${log_dir}/xgnn_pinsage_cf.log.err
 
 
+EOF
 
 
 # -----------------------
@@ -77,41 +80,41 @@ python ${sgnn_dir}/train_pinsage.py --num-worker ${num_worker} --cache-policy de
 
 dataset="twitter"
 # for gcn
-python ${sgnn_dir}/train_gcn.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop0 --gpu-extract --cache-percentage 0.10 > ${log_dir}/sgnn_gcn_tw_break.log 2> ${log_dir}/sgnn_gcn_tw_break.log.err
-python ${sgnn_dir}/train_gcn.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.81 > ${log_dir}/xgnn_gcn_tw_break.log 2> ${log_dir}/xgnn_gcn_tw_break.log.err
+python ${sgnn_dir}/train_gcn.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop0 --gpu-extract --cache-percentage 0.09 > ${log_dir}/sgnn_gcn_tw_break.log 2> ${log_dir}/sgnn_gcn_tw_break.log.err
+python ${sgnn_dir}/train_gcn.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.80 > ${log_dir}/xgnn_gcn_tw_break.log 2> ${log_dir}/xgnn_gcn_tw_break.log.err
 # for graphsage
 python ${sgnn_dir}/train_graphsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop0 --gpu-extract --cache-percentage 0.17 > ${log_dir}/sgnn_graphsage_tw_break.log 2> ${log_dir}/sgnn_graphsage_tw_break.log.err
 python ${sgnn_dir}/train_graphsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 1.00 > ${log_dir}/xgnn_graphsage_tw_break.log 2> ${log_dir}/xgnn_graphsage_tw_break.log.err
 # for pinsage
 python ${sgnn_dir}/train_pinsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --gpu-extract --cache-percentage 0.11 > ${log_dir}/sgnn_pinsage_tw_break.log 2> ${log_dir}/sgnn_pinsage_tw_break.log.err
-python ${sgnn_dir}/train_pinsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --gpu-extract --part-cache --use-dist-graph 1.0 --cache-percentage 0.85 > ${log_dir}/xgnn_pinsage_tw_break.log 2> ${log_dir}/xgnn_pinsage_tw_break.log.err
+python ${sgnn_dir}/train_pinsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --gpu-extract --part-cache --use-dist-graph 1.0 --cache-percentage 0.84 > ${log_dir}/xgnn_pinsage_tw_break.log 2> ${log_dir}/xgnn_pinsage_tw_break.log.err
 
 dataset="papers100M"
 # for gcn
 python ${sgnn_dir}/train_gcn.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop0 --gpu-extract --cache-percentage 0.08 > ${log_dir}/sgnn_gcn_pa_break.log 2> ${log_dir}/sgnn_gcn_pa_break.log.err
-python ${sgnn_dir}/train_gcn.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.71 > ${log_dir}/xgnn_gcn_pa_break.log 2> ${log_dir}/xgnn_gcn_pa_break.log.err
+python ${sgnn_dir}/train_gcn.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.70 > ${log_dir}/xgnn_gcn_pa_break.log 2> ${log_dir}/xgnn_gcn_pa_break.log.err
 # # for graphsage
 python ${sgnn_dir}/train_graphsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop0 --gpu-extract --cache-percentage 0.11 > ${log_dir}/sgnn_graphsage_pa_break.log 2> ${log_dir}/sgnn_graphsage_pa_break.log.err
-python ${sgnn_dir}/train_graphsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.83 > ${log_dir}/xgnn_graphsage_pa_break.log 2> ${log_dir}/xgnn_graphsage_pa_break.log.err
+python ${sgnn_dir}/train_graphsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.82 > ${log_dir}/xgnn_graphsage_pa_break.log 2> ${log_dir}/xgnn_graphsage_pa_break.log.err
 # # for pinsage
-python ${sgnn_dir}/train_pinsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --gpu-extract --cache-percentage 0.09 > ${log_dir}/sgnn_pinsage_pa_break.log 2> ${log_dir}/sgnn_pinsage_pa_break.log.err
-python ${sgnn_dir}/train_pinsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --gpu-extract --part-cache --use-dist-graph 1.0 --cache-percentage 0.73 > ${log_dir}/xgnn_pinsage_pa_break.log 2> ${log_dir}/xgnn_pinsage_pa_break.log.err
+python ${sgnn_dir}/train_pinsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --gpu-extract --cache-percentage 0.08 > ${log_dir}/sgnn_pinsage_pa_break.log 2> ${log_dir}/sgnn_pinsage_pa_break.log.err
+python ${sgnn_dir}/train_pinsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --gpu-extract --part-cache --use-dist-graph 1.0 --cache-percentage 0.72 > ${log_dir}/xgnn_pinsage_pa_break.log 2> ${log_dir}/xgnn_pinsage_pa_break.log.err
 
 dataset="uk-2006-05"
 # for gcn
 # pipeline will OOM
-python ${sgnn_dir}/train_gcn.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.43 > ${log_dir}/xgnn_gcn_uk_break.log 2> ${log_dir}/xgnn_gcn_uk_break.log.err
+python ${sgnn_dir}/train_gcn.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.42 > ${log_dir}/xgnn_gcn_uk_break.log 2> ${log_dir}/xgnn_gcn_uk_break.log.err
 # # for graphsage
 python ${sgnn_dir}/train_graphsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop0 --gpu-extract --cache-percentage 0.01 > ${log_dir}/sgnn_graphsage_uk_break.log 2> ${log_dir}/sgnn_graphsage_uk_break.log.err
-python ${sgnn_dir}/train_graphsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.52 > ${log_dir}/xgnn_graphsage_uk_break.log 2> ${log_dir}/xgnn_graphsage_uk_break.log.err
+python ${sgnn_dir}/train_graphsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.51 > ${log_dir}/xgnn_graphsage_uk_break.log 2> ${log_dir}/xgnn_graphsage_uk_break.log.err
 # # for pinsage
-python ${sgnn_dir}/train_pinsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --gpu-extract --part-cache --use-dist-graph 1.0 --cache-percentage 0.35 > ${log_dir}/xgnn_pinsage_uk_break.log 2> ${log_dir}/xgnn_pinsage_uk_break.log.err
+python ${sgnn_dir}/train_pinsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --gpu-extract --part-cache --use-dist-graph 1.0 --cache-percentage 0.36 > ${log_dir}/xgnn_pinsage_uk_break.log 2> ${log_dir}/xgnn_pinsage_uk_break.log.err
 
 dataset="com-friendster"
 # for gcn
-python ${sgnn_dir}/train_gcn.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.21 > ${log_dir}/xgnn_gcn_cf_break.log 2> ${log_dir}/xgnn_gcn_cf_break.log.err
+python ${sgnn_dir}/train_gcn.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.20 > ${log_dir}/xgnn_gcn_cf_break.log 2> ${log_dir}/xgnn_gcn_cf_break.log.err
 # for graphsage
-python ${sgnn_dir}/train_graphsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.96 > ${log_dir}/xgnn_graphsage_cf_break.log 2> ${log_dir}/xgnn_graphsage_cf_break.log.err
+python ${sgnn_dir}/train_graphsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.95 > ${log_dir}/xgnn_graphsage_cf_break.log 2> ${log_dir}/xgnn_graphsage_cf_break.log.err
 # for pinsage
 python ${sgnn_dir}/train_pinsage.py --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --gpu-extract --part-cache --use-dist-graph 1.0 --cache-percentage 0.28 > ${log_dir}/xgnn_pinsage_cf_break.log 2> ${log_dir}/xgnn_pinsage_cf_break.log.err
 
