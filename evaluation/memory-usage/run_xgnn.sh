ece340b0a9aa95f9b369339f927c93330e0266b1
Jiali Wang <anlarry@foxmail.com>
2023-03-11 14:32:26 +0800

    script: update app parameter

--- evaluation/memory-usage/run_xgnn.sh
+++ evaluation/memory-usage/run_xgnn.sh
@@ -22,41 +22,42 @@ xgnn_log=${log_dir}/xgnn_${model}_${ds_short}
 
 mkdir -p ${log_dir}
 
+xgnn_uk_graphsage_cache_pct=(-1 0.01 0.18 0.51 0.51)
 
 ### 1GPU ###
 log=${xgnn_log}_1wk
 python ${sgnn_dir}/train_${model}.py --num-worker 1 --cache-policy degree --batch-size 6000 \
     --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --sample-type khop3 --part-cache \
-    --gpu-extract --use-dist-graph --cache-percentage 0.01 > ${log}.log 2> ${log}.err
+    --gpu-extract --use-dist-graph 1 --cache-percentage 0.01 > ${log}.log 2> ${log}.err
 python ${sgnn_dir}/train_${model}.py --num-worker 1 --cache-policy degree --batch-size 6000 \
     --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache \
-    --gpu-extract --use-dist-graph --cache-percentage 0.01 > ${log}-bk.log 2> ${log}-bk.err
+    --gpu-extract --use-dist-graph 1 --cache-percentage 0.01 > ${log}-bk.log 2> ${log}-bk.err
 
 
 ### 2GPU ###
 log=${xgnn_log}_2wk
 python ${sgnn_dir}/train_${model}.py --num-worker 2 --cache-policy degree --batch-size 6000 \
     --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --sample-type khop3 --part-cache \
-    --gpu-extract --use-dist-graph --cache-percentage 0.09 > ${log}.log 2> ${log}.err
+    --gpu-extract --use-dist-graph 1 --cache-percentage 0.18 > ${log}.log 2> ${log}.err
 python ${sgnn_dir}/train_${model}.py --num-worker 2 --cache-policy degree --batch-size 6000 \
     --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache \
-    --gpu-extract --use-dist-graph --cache-percentage 0.09 > ${log}-bk.log 2> ${log}-bk.err
+    --gpu-extract --use-dist-graph 1 --cache-percentage 0.18 > ${log}-bk.log 2> ${log}-bk.err
 
 ### 4GPU ###
 log=${xgnn_log}_4wk
 python ${sgnn_dir}/train_${model}.py --num-worker 4 --cache-policy degree --batch-size 6000 \
     --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --sample-type khop3 --part-cache \
-    --gpu-extract --use-dist-graph --cache-percentage 0.12 > ${log}.log 2> ${log}.err
+    --gpu-extract --use-dist-graph 1 --cache-percentage 0.51 > ${log}.log 2> ${log}.err
 python ${sgnn_dir}/train_${model}.py --num-worker 4 --cache-policy degree --batch-size 6000 \
     --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache \
-    --gpu-extract --use-dist-graph --cache-percentage 0.12 > ${log}-bk.log 2> ${log}-bk.err
+    --gpu-extract --use-dist-graph 1 --cache-percentage 0.51 > ${log}-bk.log 2> ${log}-bk.err
 
 
 ### 8GPU ###
 log=${xgnn_log}_8wk
 python ${sgnn_dir}/train_${model}.py --num-worker 8 --cache-policy degree --batch-size 6000 \
     --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --sample-type khop3 --part-cache \
-    --gpu-extract --use-dist-graph --cache-percentage 0.12 > ${log}.log 2> ${log}.err
+    --gpu-extract --use-dist-graph 1 --cache-percentage 0.51 > ${log}.log 2> ${log}.err
 python ${sgnn_dir}/train_${model}.py --num-worker 8 --cache-policy degree --batch-size 6000 \
     --num-epoch ${num_epoch} --dataset ${dataset} --sample-type khop3 --part-cache \
-    --gpu-extract --use-dist-graph --cache-percentage 0.12 > ${log}-bk.log 2> ${log}-bk.err
+    --gpu-extract --use-dist-graph 1 --cache-percentage 0.51 > ${log}-bk.log 2> ${log}-bk.err
