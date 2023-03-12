617ee25f42e96c636118adc1befbe9686266993b
Dahai Tang <seatang@hnu.edu.cn>
2023-03-12 21:13:40 +0800

    update scripts

--- evaluation/acc_test/run.sh
+++ evaluation/acc_test/run.sh
@@ -18,5 +18,5 @@ num_epoch=200
 
 # papers100M acc: 56%
 python ${dgl_dir}/train_graphsage.py --batch-size 6000 --dataset papers100M --pipelining --report-acc 151 --num-epoch ${num_epoch} --use-gpu-sampling --use-uva-feat --devices ${dgl_devices} > ${log_dir}/dgl_graphsage_pa.log 2>&1
-# python ${sgnn_dir}/train_graphsage.py --report-acc 151 --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --sample-type khop0 --gpu-extract --cache-percentage 0.11 > ${log_dir}/sgnn_graphsage_pa.log 2>&1
-# python ${sgnn_dir}/train_graphsage.py --report-acc 151 --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --sample-type khop3 --part-cache --gpu-extract --use-dist-graph --cache-percentage 0.20 > ${log_dir}/xgnn_graphsage_pa.log 2>&1
+python ${sgnn_dir}/train_graphsage.py --report-acc 151 --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --sample-type khop0 --gpu-extract --cache-percentage 0.11 > ${log_dir}/sgnn_graphsage_pa.log 2>&1
+python ${sgnn_dir}/train_graphsage.py --report-acc 151 --num-worker ${num_worker} --cache-policy degree --batch-size 6000 --num-epoch ${num_epoch} --dataset ${dataset} --pipeline --sample-type khop3 --part-cache --gpu-extract --use-dist-graph 1.0 --cache-percentage 0.80 > ${log_dir}/xgnn_graphsage_pa.log 2>&1
